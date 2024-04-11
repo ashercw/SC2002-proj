493eb7c46a756abcf6e973f4dd58f93eb8a277d2
akneehao <87539145+akneehao@users.noreply.github.com>
2024-04-12 01:49:12 +0700

    Update ReadyToPickUpList.java

--- FOMS/src/Boundary/ReadyToPickUpList.java
+++ FOMS/src/Boundary/ReadyToPickUpList.java
@@ -1,8 +1,8 @@
-
 package Boundary;
+import java.util.Set;
+import java.util.HashSet;
 public class ReadyToPickUpList {
-
-	private String orderID;
+	private static final Set<Integer> DisplayOrder = new HashSet<>(); //orderID stored in Hashset
 
 	/**
 	 * 
@@ -10,6 +10,13 @@ public class ReadyToPickUpList {
 	 */
 	public void displayReadyToPickUpList(int orderID) {
 		// TODO - implement ReadyToPickUpList.displayReadyToPickUpList
+		if(DisplayOrder.isEmpty()){
+			System.out.println("Order(s) are not ready to get picked up!");
+			return;
+		}else{
+			System.out.println("Order(s) are ready to be picked up!");
+
+		}
 		throw new UnsupportedOperationException();
 	}
 
@@ -19,6 +26,8 @@ public class ReadyToPickUpList {
 	 */
 	public void addReadyToPickUpList(int orderID) {
 		// TODO - implement ReadyToPickUpList.addReadyToPickUpList
+		DisplayOrder.add(orderID);
+		System.out.println("Order: " + DisplayOrder + "added to 'Ready to be picked up' List");
 		throw new UnsupportedOperationException();
 	}
 
@@ -28,7 +37,13 @@ public class ReadyToPickUpList {
 	 */
 	public void deleteReadyToPickUpList(int orderID) {
 		// TODO - implement ReadyToPickUpList.deleteReadyToPickUpList
+		if(DisplayOrder.remove(orderID)){
+			System.out.println("Order" + orderID + "is removed from the list");
+		}else{
+			System.out.println("Order" + orderID + "is not found in the list. Please choose orders in the list!");
+		}
 		throw new UnsupportedOperationException();
 	}
 
 }
+
