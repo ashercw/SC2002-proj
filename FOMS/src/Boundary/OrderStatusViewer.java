7c8f185f467c528d7313d18bd78b5f8996d8e3be
akneehao <87539145+akneehao@users.noreply.github.com>
2024-04-12 01:49:38 +0700

    Update OrderStatusViewer.java

--- FOMS/src/Boundary/OrderStatusViewer.java
+++ FOMS/src/Boundary/OrderStatusViewer.java
@@ -1,15 +1,25 @@
 package Boundary;
+import java.util.HashMap;
+import java.util.Map;
 
 public class OrderStatusViewer {
+	private static final Map<Integer, String> Status = new HashMap<>(); //Stores the status of list of orderID
+	// Integer represents orderID
+	// String represents Status
 
-	private String orderID;
 
 	/**
 	 * 
 	 * @param orderID
 	 */
-	public void displayOrderStatus(int orderID) {
+	public static void displayOrderStatus(int orderID) {
 		// TODO - implement OrderStatusViewer.displayOrderStatus
+		if(!Status.containsKey(orderID)){  //orderID does not exist
+			System.out.println("Order ID Not Found!");
+			return;
+		}else{
+			System.out.println(orderID + ": " + Status.get(orderID)); 
+		}
 		throw new UnsupportedOperationException();
 	}
 
@@ -17,8 +27,10 @@ public class OrderStatusViewer {
 	 * 
 	 * @param orderID
 	 */
-	public void changeOrderStatus(int orderID) {
+	public static void changeOrderStatus(int orderID, String newStatus) {
 		// TODO - implement OrderStatusViewer.changeOrderStatus
+		Status.put(orderID, newStatus); //changes order status
+		System.out.println(orderID + "status changed to: " + newStatus);
 		throw new UnsupportedOperationException();
 	}
 
