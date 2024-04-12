08eb2a74383e1bcb1c15dc23edfba7ce316f32fa
Ripleyyyyy <10203191h@gmail.com>
2024-04-12 22:14:41 +0800

    menu list by branch repo!

--- FOMS/src/Controller/Menu/MenuControllerTemp.java
+++ FOMS/src/Controller/Menu/MenuControllerTemp.java
@@ -1,20 +1,18 @@
 package Controller.Menu;
 
-import Entity.BranchType;
 import Entity.Food.FoodItem;
 import Entity.Food.ItemType;
-import Entity.User.*;
 import java.util.List;
 import java.io.IOException;
 import java.util.ArrayList;
 import Others.IO;
 import Others.TextDB;
-import Boundary.AttributeGetter;
 
 /**
  * Temporary because idt i can run the current menucontroller coz of code issues
  * @author Saffron Lim;
  */
+@SuppressWarnings({"unchecked", "rawtypes"})
 public class MenuControllerTemp {
 
     public static String FILEPATH = "FOMS\\src\\Others\\menu_listCSV.csv";
@@ -24,12 +22,14 @@ public class MenuControllerTemp {
      * respective FoodItem entity objects.
      */
 
+    
     public static void loadFoodItems() {
+        
         List al = new ArrayList();
 
-        List<List<String>> empList = IO.readCSV(FILEPATH);
+        List<List<String>> foodList = IO.readCSV(FILEPATH);
 
-        for (List<String> str : empList) {
+        for (List<String> str : foodList) {
             String name = str.get(0);
             name = IO.toDisplayCase(name);
             double price = Double.parseDouble(str.get(1));
@@ -52,13 +52,40 @@ public class MenuControllerTemp {
 
         }
         // add to repo
-        addToFoodRepo("FoodItemRepo", al);
+        addToFoodRepo("FoodItemRepo.txt", al);
+        loadMenuitems(al);
 
+    }
+
+
+    public static void loadMenuitems(List al)
+    {
+        try {
+			ArrayList branchList = TextDB.readBranch("BranchRepo.txt", false);
+
+			for (int i = 0; i < branchList.size(); i++) {
+				List menuList = new ArrayList();
+				String branchName = (String) branchList.get(i);
+				String FILENAME = branchName + "MenuListRepo.txt";
+				for(int j = 0; j< al.size(); j++)
+				{
+                    FoodItem foodObj = (FoodItem) al.get(j);
+					String branch = foodObj.getFoodItemBranch();
+					if(branch.equals(branchName))
+					{
+						menuList.add(foodObj);
+					}
+				}
+				TextDB.saveFood(FILENAME, menuList);
+
+			}
+		} catch (IOException e) {
+			System.out.println("IOException > " + e.getMessage());
+		}
     }
     
 
     public static void addToFoodRepo(String FILEPATH, List al) {
-		TextDB txtDB = new TextDB();
 		try {
 			
 			TextDB.saveFood(FILEPATH, al);
