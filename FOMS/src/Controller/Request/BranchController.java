8714687c52b168386348897ffadb6e0aa4a677e2
Ripleyyyyy <10203191h@gmail.com>
2024-04-17 19:08:26 +0800

    fixed issue of deleted BranchController code

--- FOMS/src/Controller/Request/BranchController.java
+++ FOMS/src/Controller/Request/BranchController.java
@@ -4,92 +4,97 @@ import Entity.Branch;
 import Entity.User.Staff;
 import Entity.User.Manager;
 import java.util.List;
+
+import Controller.Account.AccountController;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import Others.IO; // Assume IO operations for reading/writing data
+import Others.TextDB;
 import Others.TextDBBranch;
+import Others.TextDBStaff;
+
+
 
 public class BranchController {
+
     private List<Branch> branches = new ArrayList<>();
+    public static String FILEPATH = "FOMS\\src\\Others\\branch_listCSV.csv";
 
-    public BranchController() {
-        loadBranches(); // Loads branches from storage
+
+    /**
+     * Loads branch list from branch list CSV file. Additionally initialises the
+     * respective Branch entity objects.
+     * Loads branch list from branch list CSV file. Additionally initialises the
+     * respective Branch entity objects.
+     * INCOMPLETE
+     */
+
+    public static void loadBranches() {
+        List al = new ArrayList();
+
+        List<List<String>> empList = IO.readCSV(FILEPATH);
+
+        for (List<String> str : empList) {
+            String name = str.get(0);
+            String location = str.get(1);
+            int quota = Integer.parseInt(str.get(2));
+
+            Branch branchObj = new Branch(name, location, quota);
+            al.add(branchObj);
+
+        }
+        // add to repo
+        addToBranchRepo("BranchRepo.txt", al);
     }
 
-    public void addStaffToBranch(String staffId, String branchName) {
-        // Find the branch
-        Branch branch = branches.stream()
-            .filter(b -> b.getBranchName().equals(branchName))
-            .findFirst()
-            .orElse(null);
 
-        if (branch != null) {
-            // Assuming staff details are fetched or created elsewhere
-            Staff newStaff = fetchStaffById(staffId); // Implement fetchStaffById according to your data source
-            if (newStaff != null) {
-                List<Staff> staffList = branch.getStaffList();
-                if (!staffList.contains(newStaff)) {
-                    staffList.add(newStaff);
-                    newStaff.setBranch(branchName);
-                    saveBranches();  // Save updates
+    /**
+     * This function writes branch data to a txt file for data storage purposes.
+     * 
+     * @param FILEPATH is the String directory of the file to write to.
+     * @param al       is a List of Branch objects whose content will be saved in
+     *                 the txt file.
+     * @param al       is a List of Branch objects whose content will be saved in
+     *                 the txt file.
+     * 
+     */
+    public static void addToBranchRepo(String FILEPATH, List al) {
+        try {
+
+            TextDBBranch.saveBranch(FILEPATH, al);
+        } catch (IOException e) {
+            System.out.println("IOException > " + e.getMessage());
+        }
+    }
+
+    /**
+     * 
+     * @param FILEPATH
+     * @param al       is a List of employee objects
+     * 
+     */
+    public static void createStaffListbyBranch(List<List<String>> empList) {
+
+        try {
+            ArrayList branchList = TextDBBranch.readBranch("BranchRepo.txt", false);
+
+            for (int i = 0; i < branchList.size(); i++) {
+                List childBranchList = new ArrayList();
+                System.out.println(branchList.get(i));
+                String branchName = (String) branchList.get(i);
+                String FILENAME = branchName + "StaffListRepo.txt";
+                for (List<String> str : empList) {
+                    String role = str.get(2);
+                    if (!role.equals("A") && str.get(5).equals(branchName)) {
+                        childBranchList.add(AccountController.createEmployeeObj(str, role));
+                    }
                 }
+                TextDBStaff.saveEmployee(FILENAME, childBranchList);
+
             }
+        } catch (IOException e) {
+            System.out.println("IOException > " + e.getMessage());
         }
-    }
-
-    public void promoteStaffToManager(String staffId, String branchName) {
-        Branch branch = branches.stream()
-            .filter(b -> b.getBranchName().equals(branchName))
-            .findFirst()
-            .orElse(null);
-
-        if (branch != null) {
-            Staff staff = branch.getStaffList().stream()
-                .filter(s -> s.getLoginID().equals(staffId))
-                .findFirst()
-                .orElse(null);
-
-            if (staff != null && !staff.getPromotion()) {
-                staff.setPromotion(true);
-                // Assuming Manager extends Staff
-                Manager newManager = new Manager(staff.getEmployeeName(), staff.getLoginID(), staff.getEmployeeType(),
-                                                staff.getGender(), staff.getAge(), branchName, staff.getPassword());
-                branch.getManagerList().add(newManager);
-                branch.getStaffList().remove(staff);
-                saveBranches();
-            }
-        }
-    }
-
-    public void transferStaff(String staffId, String fromBranchName, String toBranchName) {
-        Branch fromBranch = branches.stream()
-            .filter(b -> b.getBranchName().equals(fromBranchName))
-            .findFirst()
-            .orElse(null);
-        Branch toBranch = branches.stream()
-            .filter(b -> b.getBranchName().equals(toBranchName))
-            .findFirst()
-            .orElse(null);
-
-        if (fromBranch != null && toBranch != null) {
-            Staff staff = fromBranch.getStaffList().stream()
-                .filter(s -> s.getLoginID().equals(staffId))
-                .findFirst()
-                .orElse(null);
-
-            if (staff != null) {
-                fromBranch.getStaffList().remove(staff);
-                toBranch.getStaffList().add(staff);
-                staff.setBranch(toBranchName);
-                saveBranches();
-            }
-        }
-    }
-
-    private void loadBranches() {
-        // Implement loading logic
-    }
-
-    private void saveBranches() {
-        // Implement saving logic
     }
 }
