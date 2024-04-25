b639c4cb3a4381c5f823bcbc4d0d0bc6cc3f5942
ashercw <105447494+ashercw@users.noreply.github.com>
2024-04-25 12:23:53 +0800

    changed some OrderLine.java accessors to debug ordercontroller
    orderid and setcustomisation

--- FOMS/src/Entity/Order/OrderLine.java
+++ FOMS/src/Entity/Order/OrderLine.java
@@ -4,37 +4,45 @@ import Entity.Food.FoodItem;
 
 public class OrderLine {
     private FoodItem item;
-	private int itemQuantity;
-	private Customisation custom;
-	private String customOrder;
+    private int itemQuantity;
+    private Customisation custom;
+    private int orderId;
 
-	//Constructor
-	public OrderLine(FoodItem _f, int _q, Customisation _c, String _co)
-	{
-		this.item = _f;
-		this.itemQuantity = _q;
-		this.custom = _c;
-		this.customOrder = _co;
-	}
-	
-	//Accessors and mutators
-	public FoodItem getItem()
-	{
-		return this.item;
-	}
-	
-	public int getitemQuanity()
-	{
-		return this.itemQuantity;
-	}
-	
-	public Customisation getCustom()
-	{
-		return this.custom;
-	}
-	
-	public String getCustomOrder()
-	{
-		return this.customOrder;
-	}
+    public OrderLine(FoodItem _f, int _q, Customisation _c) {
+        this.item = _f;
+        this.itemQuantity = _q;
+        this.custom = _c;
+    }
+
+    public FoodItem getItem() {
+        return this.item;
+    }
+
+    public void setItem(FoodItem item) {
+        this.item = item;
+    }
+
+    public int getItemQuantity() {
+        return this.itemQuantity;
+    }
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+    public Customisation getCustomisation() {
+        return this.custom;
+    }
+
+    public void setCustomisation(Customisation custom) {
+        this.custom = custom;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
 }
