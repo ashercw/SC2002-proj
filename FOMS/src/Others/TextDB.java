f042e66db2d4982cc605b67fe7c05d98eec95b1c
Ripleyyyyy <10203191h@gmail.com>
2024-04-25 10:55:02 +0800

    modifyFile function in TextDB

--- FOMS/src/Others/TextDB.java
+++ FOMS/src/Others/TextDB.java
@@ -59,4 +59,56 @@ public class TextDB {
         return data;
     }
 
+
+    /**
+     * This function modifies a specific string in a .txt file.
+     * @param filePath String with the file path.
+     * @param oldString String in the .txt file that needs to be replaced.
+     * @param newString String to replace oldString with.
+     * 
+     */
+    static void modifyFile(String filePath, String oldString, String newString)
+    {
+        File fileToBeModified = new File(filePath);
+        String oldContent = "";
+        BufferedReader reader = null;
+        FileWriter writer = null;
+         
+        try
+        {
+            reader = new BufferedReader(new FileReader(fileToBeModified));
+            //Reading all the lines of input text file into oldContent
+            String line = reader.readLine();
+            while (line != null) 
+            {
+                oldContent = oldContent + line + System.lineSeparator();
+                line = reader.readLine();
+            }
+             
+            //Replacing oldString with newString in the oldContent
+            String newContent = oldContent.replaceAll(oldString, newString);
+            //Rewriting the input text file with newContent
+            writer = new FileWriter(fileToBeModified);
+            writer.write(newContent);
+        }
+        catch (IOException e)
+        {
+
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                //Closing the resources
+                reader.close();
+                writer.close();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
