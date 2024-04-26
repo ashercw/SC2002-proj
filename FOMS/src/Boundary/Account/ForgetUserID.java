2a3f7d341d7d100599754c0a360dedae2115828f
akneehao <87539145+akneehao@users.noreply.github.com>
2024-04-27 01:18:11 +0700

    Delete FOMS/src/Boundary/Account/ForgetUserID.java

--- FOMS/src/Boundary/Account/ForgetUserID.java
+++
@@ -1,9 +0,0 @@
-package Boundary.Account;
-public class ForgetUserID {
-
-	public void forgetID() {
-		// TODO - implement ForgetUserID.forgetID
-		throw new UnsupportedOperationException();
-	}
-
-}
