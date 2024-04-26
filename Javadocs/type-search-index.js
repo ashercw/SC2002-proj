4227a0e9ee5ed893359ec609a39ff8d9be426c5c
Ripleyyyyy <161046482+Ripleyyyyy@users.noreply.github.com>
2024-04-27 07:36:31 +0800

    Add files via upload

---
+++ Javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Others","l":"FileNames"},{"p":"Others","l":"IO"},{"p":"Others","l":"TextDB"},{"p":"Others","l":"TextDBBranch"},{"p":"Others","l":"TextDBFood"},{"p":"Others","l":"TextDBOrder"},{"p":"Others","l":"TextDBPayment"},{"p":"Others","l":"TextDBStaff"}];updateSearchResults();
